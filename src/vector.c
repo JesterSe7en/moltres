73888456308aa84bd41aeebb024ae12a3ef44309
Alex Chan <alyxchan87@gmail.com>
2024-02-10 21:21:57 -0800

    Add Vector2i - integer version
    Use compound literal to create and return a Vector2i

--- src/vector.c
+++ src/vector.c
@@ -4,3 +4,6 @@ Vector2F v2f(float x, float y) {
   Vector2F v = {x, y};
   return v;
 }
+
+// another way to return back a vector via compound literal
+Vector2i v2i(int x, int y) { return (Vector2i){.x = x, .y = y}; }
--- src/vector.h
+++ src/vector.h
@@ -5,6 +5,11 @@ typedef struct Vector2F {
   float x, y;
 } Vector2F;
 
+typedef struct Vector2i {
+  int x, y;
+} Vector2i;
+
 Vector2F v2f(float x, float y);
+Vector2i v2i(int x, int y);
 
 #endif // INCLUDE_SRC_VECTOR_H_
