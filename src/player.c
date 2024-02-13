78c9e6c3f1c5d3e0fe2b525f722895013a1295ba
Alex Chan <alyxchan87@gmail.com>
2024-02-13 15:54:53 -0800

    Example of using hashtable

--- src/player.c
+++ src/player.c
@@ -9,6 +9,22 @@ Player player_create(Vector2f *spawn_point) {
 
   player.animations = hashtable_create();
 
+  HashTable *ht = player.animations;
+
+  printf("adding animations to hashtable\n");
+  hashtable_add(ht, "idle", "heheh im idling...");
+  printf("added idle\n");
+  printf("adding runinng to hashtable\n");
+  hashtable_add(ht, "running", "heheh im running  pee pee poo poo...");
+  printf("added running\n");
+  printf("adding jumping to hashtable\n");
+  hashtable_add(ht, "jumping", "heheh im jumping...");
+  printf("adding done\n");
+
+  // search for running key, print out the value
+  printf("search for running: message = %s\n",
+         (const char *)hashtable_get(ht, "running"));
+
   player.current_animation = NULL;
 
   printf("Size of player in bytes: %lu bytes\n", sizeof(Player));
