d79308c3b19b63c837dd40e35bb32aec4432387a
Alex Chan <alyxchan87@gmail.com>
2024-02-13 16:47:43 -0800

    Remove example code for hashtable

--- src/player.c
+++ src/player.c
@@ -5,29 +5,18 @@
 
 Player player_create(Vector2f *spawn_point) {
   Player player;
+
   player.spawn_point = *spawn_point;
-
   player.animations = hashtable_create();
-
-  HashTable *ht = player.animations;
-
-  printf("adding animations to hashtable\n");
-  hashtable_add(ht, "idle", "heheh im idling...");
-  printf("added idle\n");
-  printf("adding runinng to hashtable\n");
-  hashtable_add(ht, "running", "heheh im running  pee pee poo poo...");
-  printf("added running\n");
-  printf("adding jumping to hashtable\n");
-  hashtable_add(ht, "jumping", "heheh im jumping...");
-  printf("adding done\n");
-
-  // search for running key, print out the value
-  printf("search for running: message = %s\n",
-         (const char *)hashtable_get(ht, "running"));
-
   player.current_animation = NULL;
 
-  printf("Size of player in bytes: %lu bytes\n", sizeof(Player));
-
   return player;
+}
+
+void player_addAnimation(Player *player, const char *key,
+                         AnimationInfo ani_info) {
+  // TODO: implement add animation
+  printf("Adding animation to player\n");
+
+  return;
 }
