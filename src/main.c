0f969a1ab36d0603fea862e6ac75ab4dc4104023
Alex Chan <alyxchan87@gmail.com>
2024-02-14 20:09:17 -0800

    Remove player; restructure includes in main

--- src/main.c
+++ src/main.c
@@ -1,16 +1,4 @@
 #include "main.h"
-#include "entity.h"
-#include "player.h"
-#include "vector.h"
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_video.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 static int last_frame_time = 0;
 
--- src/main.h
+++ src/main.h
@@ -1,3 +1,14 @@
+#include "entity.h"
 #include "renderwindow.h"
+#include "vector.h"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
+
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
