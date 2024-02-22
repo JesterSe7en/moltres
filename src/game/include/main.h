0fbbc0a6c5ee0bcf8e33b1b0f1d128771135963a
Alex Chan <alyxchan87@gmail.com>
2024-02-22 14:19:30 -0800

    Create basic executable game
    Does not use moltres engine.  Simple hello world

---
+++ src/game/include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by alyxc on 2/22/2024.
+//
+
+#ifndef MOLTRES_SRC_GAME_SRC_MAIN_H_
+#define MOLTRES_SRC_GAME_SRC_MAIN_H_
+
+#include <stdio.h>
+
+#endif //MOLTRES_SRC_GAME_SRC_MAIN_H_
---
+++ src/game/src/main.c
@@ -0,0 +1,10 @@
+//
+// Created by alyxc on 2/22/2024.
+//
+
+#include "main.h"
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
