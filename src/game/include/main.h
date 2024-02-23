0536e0f08cd68c293054eb3925f6ec3f8ca00e6c
Alex Chan <alyxchan87@gmail.com>
2024-02-22 22:59:12 -0800

    Remove comments

--- src/game/include/main.h
+++ src/game/include/main.h
@@ -1,7 +1,3 @@
-//
-// Created by alyxc on 2/22/2024.
-//
-
 #ifndef MOLTRES_SRC_GAME_SRC_MAIN_H_
 #define MOLTRES_SRC_GAME_SRC_MAIN_H_
 
--- src/game/src/main.c
+++ src/game/src/main.c
@@ -1,10 +1,6 @@
-//
-// Created by alyxc on 2/22/2024.
-//
-
 #include "main.h"
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+  printf("Hello, World!\n");
+  return 0;
 }
